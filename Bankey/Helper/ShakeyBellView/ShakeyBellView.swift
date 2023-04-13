a70bbbd2fd79613d022fc7679b5a815a1d669ca6
baris dilekci <bdilekci200@gmail.com>
2023-04-14 02:36:17 +0300

    feat: Adding the badge

--- Bankey/Helper/ShakeyBellView/ShakeyBellView.swift
+++ Bankey/Helper/ShakeyBellView/ShakeyBellView.swift
@@ -10,8 +10,14 @@ import UIKit
 
 class ShakeyBellView: UIView {
     
+    //MARK: - UI Elements
+    let buttonView = UIButton()
     let imageView = UIImageView()
     
+    //MARK: - Properties
+    let buttonHeight: CGFloat = 16
+    
+    //MARK: - Lifecycle
     override init(frame: CGRect) {
         super.init(frame: frame)
         style()
@@ -27,6 +33,7 @@ class ShakeyBellView: UIView {
     }
 }
 
+//MARK: - Helper
 extension ShakeyBellView {
     
     func style() {
@@ -39,16 +46,34 @@ extension ShakeyBellView {
         let singleTap = UITapGestureRecognizer(target: self, action: #selector(imageViewTapped(_: )))
         imageView.addGestureRecognizer(singleTap)
         imageView.isUserInteractionEnabled = true
+        
+        
+        buttonView.translatesAutoresizingMaskIntoConstraints = false
+        buttonView.backgroundColor = .systemRed
+        buttonView.titleLabel?.font = UIFont.systemFont(ofSize: 13)
+        buttonView.layer.cornerRadius = buttonHeight/2
+        buttonView.setTitle("9", for: .normal)
+        buttonView.setTitleColor(.white, for: .normal)
     }
     
     func layout() {
         addSubview(imageView)
+        addSubview(buttonView)
         
+        // image
         NSLayoutConstraint.activate([
             imageView.centerXAnchor.constraint(equalTo: centerXAnchor),
             imageView.centerYAnchor.constraint(equalTo: centerYAnchor),
             imageView.heightAnchor.constraint(equalToConstant: 24),
             imageView.widthAnchor.constraint(equalToConstant: 24)
+        ])
+        
+        // Button
+        NSLayoutConstraint.activate([
+            buttonView.topAnchor.constraint(equalTo: imageView.topAnchor),
+            buttonView.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: -9),
+            buttonView.widthAnchor.constraint(equalToConstant: 16),
+            buttonView.heightAnchor.constraint(equalToConstant: 16)
         ])
     }
 }
@@ -63,15 +88,15 @@ extension ShakeyBellView {
 
 // MARK: - Animations
 extension ShakeyBellView {
- 
+    
     private func shakeWith(duration: Double, angle: CGFloat, yOffset: CGFloat) {
         let numberOfFrames: Double = 6
         let frameDuration = Double(1/numberOfFrames)
         
         imageView.setAnchorPoint(CGPoint(x: 0.5, y: yOffset))
-
+        
         UIView.animateKeyframes(withDuration: duration, delay: 0, options: [],
-          animations: {
+                                animations: {
             UIView.addKeyframe(withRelativeStartTime: 0.0,
                                relativeDuration: frameDuration) {
                 self.imageView.transform = CGAffineTransform(rotationAngle: -angle)
@@ -96,8 +121,8 @@ extension ShakeyBellView {
                                relativeDuration: frameDuration) {
                 self.imageView.transform = CGAffineTransform.identity
             }
-          },
-          completion: nil
+        },
+                                completion: nil
         )
     }
 }
@@ -106,18 +131,18 @@ extension UIView {
     func setAnchorPoint(_ point: CGPoint) {
         var newPoint = CGPoint(x: bounds.size.width * point.x, y: bounds.size.height * point.y)
         var oldPoint = CGPoint(x: bounds.size.width * layer.anchorPoint.x, y: bounds.size.height * layer.anchorPoint.y)
-
+        
         newPoint = newPoint.applying(transform)
         oldPoint = oldPoint.applying(transform)
-
+        
         var position = layer.position
-
+        
         position.x -= oldPoint.x
         position.x += newPoint.x
-
+        
         position.y -= oldPoint.y
         position.y += newPoint.y
-
+        
         layer.position = position
         layer.anchorPoint = point
     }
