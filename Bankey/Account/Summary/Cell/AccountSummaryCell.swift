86dbd318224c3288dc156309277ebc5dac33f494
baris dilekci <bdilekci200@gmail.com>
2023-04-14 01:28:22 +0300

    fix:  the label spacing

--- Bankey/Account/Summary/Cell/AccountSummaryCell.swift
+++ Bankey/Account/Summary/Cell/AccountSummaryCell.swift
@@ -71,6 +71,7 @@ extension AccountSummaryCell {
         
         nameLabel.translatesAutoresizingMaskIntoConstraints = false
         nameLabel.font = UIFont.preferredFont(forTextStyle: .body)
+        nameLabel.adjustsFontSizeToFitWidth = true
         nameLabel.text = "Account name"
         
         balanceStackView.translatesAutoresizingMaskIntoConstraints = false
@@ -80,10 +81,12 @@ extension AccountSummaryCell {
         balanceLabel.translatesAutoresizingMaskIntoConstraints = false
         balanceLabel.font = UIFont.preferredFont(forTextStyle: .body)
         balanceLabel.textAlignment = .right
+        balanceLabel.adjustsFontSizeToFitWidth = true
         balanceLabel.text = "Some balance"
 
         balanceAmountLabel.translatesAutoresizingMaskIntoConstraints = false
         balanceAmountLabel.textAlignment = .right
+        balanceAmountLabel.adjustsFontSizeToFitWidth = true
         balanceAmountLabel.text = "$929,466.63"
         
         chevronImageView.translatesAutoresizingMaskIntoConstraints = false
